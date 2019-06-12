67db4e6d7dc79c18ebf4d991e61025260cbebd77
FreeYewb <1319192521@qq.com>
2019-06-12 14:32:17 +0800

    重新创建test文件，改为junit4

--- src/com/spring4/jdbc/JDBCTest.java
+++ src/com/spring4/jdbc/JDBCTest.java
@@ -1,22 +1,36 @@
 package com.spring4.jdbc;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.sql.SQLException;
 
 import javax.sql.DataSource;
-
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+/**
+ * 
+* @Title: testDataSource  
+* @Description: TODO(SPRING JDBC)  
+* @param @throws SQLException    参数  
+* @return void    返回类型  
+* @throws
+ */
+public class JDBCTest {
 
-class JDBCTest {
-	
 	private ApplicationContext ctx = null;
-	
+	private JdbcTemplate jdbcTemplate;
 	{
 		ctx = new ClassPathXmlApplicationContext("applicationContext4.xml");
+		jdbcTemplate = (JdbcTemplate) ctx.getBean("jdbcTemplate");
 	}
+	@Test
+	public void testUpdate() {
+		String sql = "UPDATE user_t SET afe = ? WHERE ID = ?";
+		
+		jdbcTemplate.update(sql,8,1);
+	}
+	
 	
 	@Test
 	public void testDataSource() throws SQLException {
@@ -25,3 +39,8 @@ class JDBCTest {
 	}
 
 }
+
+
+
+
+
